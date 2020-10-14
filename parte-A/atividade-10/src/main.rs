9ba558a916bdbcdfaa1827347e685f64fb6c5042
KarolinyPessoal <karolinycosta@yahoo.com.br>
2020-10-14 14:50:00 -0300

    Atualização das atividades

--- parte-A/atividade-10/src/main.rs
+++ parte-A/atividade-10/src/main.rs
@@ -1,3 +1,18 @@
+//Ler um valor numérico inteiro e apresente uma 
+//mensagem informando se o valor fornecido é par ou ímpar
+
+
 fn main() {
-    println!("Hello, world!");
+
+    let number = 13;
+
+    if number % 2 == 0 {
+        println!("O número que você digitou é par {}",number );
+    }
+
+   else {
+        println!("O número que você digitou é ímpar {}",number );
+    }
+    
+    
 }

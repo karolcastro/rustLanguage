31df5c33135210260622c4b0b050d333c46206da
KarolinyPessoal <karolinycosta@yahoo.com.br>
2020-11-30 10:05:25 -0300

    exercicio 9

--- parte-B/atividade-9/src/main.rs
+++ parte-B/atividade-9/src/main.rs
@@ -2,18 +2,45 @@
 /// 
 /// Elaborar um programa que leia 10 elementos (valores reais) para temperatura em graus Celsius e armazene esses valores em um vetor A. O programa ao final deve apresentar a menor, a maior e a média das temperaturas lidas.
 
+
 fn main() {
-    let temp = [34.5, 32.5, 28.3, 25.4, 30.0, 31.0, 33.3, 26.6, 27.6, 27.8];
-    let mut contador = 0;
-    let media = (temp[0] + temp[1] + temp[2] + temp[3] + temp[4] + temp[5] + temp[6] + temp[7] + temp[8] + temp[9]) / 10.0;
-   
 
-    while contador < 10 {
-        println!(" A média das temperaturas é: { } ",media);
-        println!(" A maior temperaturas é: { } ",temp.len());
-        println!(" A menor temperatura é: { } ",media);
-        break;
-    }
+    let temp1 = &[34.5, 32.5, 28.3, 25.4, 30.0, 31.0, 33.3, 26.6, 27.6, 27.8];
+    let  mut contador :f32 = 0.0;
+
+    let mut menor_temperatura :f32 = temp1[0];
+    let mut maior_temperatura :f32 = temp1[0];
     
+   
+    //calcular menor
+    for i in temp1.iter(){
+        if i < &menor_temperatura {
+            menor_temperatura = *i;
+        } 
+    }
 
+    //calcular maior
+    for i in temp1.iter(){
+        if i > &maior_temperatura {
+            maior_temperatura = *i;
+        } 
+    }
+   
+    //calcular a média
+    for i in temp1.iter(){
+        contador = contador + i;
+    }
+    let media_temperatura :f32 = contador / 10.0;
+
+    //Ler todas as temperaturas
+    println!("A menor temperatura é = {}, A maior é = {} e a média é = {}.", menor_temperatura,maior_temperatura, media_temperatura);
+    
+    
 }
+
+// fn converter_string_para_i32(uma_string: String) -> i32 {
+//     uma_string
+//         .trim()
+//         .parse()
+//         .expect("Falha ao converter o valor")
+// }

68a74bc4ed18ac3ec919a667cd5fd8173e899de4
KarolinyPessoal <karolinycosta@yahoo.com.br>
2020-12-28 10:32:32 -0300

    add atividade-5B correta

--- parte-B/atividade-5B/src/main.rs
+++ parte-B/atividade-5B/src/main.rs
@@ -1,14 +1,19 @@
+//Elaborar um programa que leia 3 valores inteiros, armazene-os em um vetor, e percorra o vetor calculando o fatorial de cada valor lido. No final, deverá apresentar o somatórios dos valores calculados.
+
+//A partir do exercício 5, os próximos deverão utilizar vetores para armazenar os valores lidos.
+
 fn main() {
 
     let mut contador = 1;
     let mut resultado_fatorial = 1;
 
-    while contador <= 5 {
+    while contador <= 2
+     {
 
         resultado_fatorial = resultado_fatorial * contador;
         contador = contador + 1;
     }
-    
+    // esta retornando outro valor ... 120
     println!("O resultado é {:?}", resultado_fatorial)
 
 }

15c185d4b72350ab9d528ca49faf28b6d6e28749
KarolinyPessoal <karolinycosta@yahoo.com.br>
2020-12-28 10:27:18 -0300

    add atividade-11 correta

--- parte-B/atividade-12/src/main.rs
+++ parte-B/atividade-12/src/main.rs
@@ -2,17 +2,44 @@
 
 //Elaborar um programa que leia um vetor A com dez elementos numéricos inteiros. Apresentar o total de elementos ímpares existente no vetor e também o percentual do valor total de números ímpares em relação à quantidade total de elementos armazenados.
 
-fn main() {
-let mut numero: i32 = 0; 
-let mut total_impar = 0;
-    
-  while numero <= 10{
-      if numero %2 == 1{
-          total_impar = total_impar+1;
+use std::io;
 
-      }
-      numero +=1;
+fn main() {
+
+  let  mut vetor_a = [0;10];
+  let mut contador = 0;
+  let mut sum:i32 = vetor_a.iter().sum();
+  // let total_impar:i64 = [0; 10].iter().sum();
+  // let  total_impar:i64 = vetor_a.sum();
+
+  while contador < vetor_a.len() {
+
+    println!("Informe 10 números: ");
+
+  let mut string_numero: String = String::new();
+  io::stdin()
+    .read_line(&mut string_numero)
+    .expect("Falha ao ler o valor");
+  
+  let numero: i32 = converter_string_para_i32(string_numero);
+    
+    if numero % 2 == 1 {
+      vetor_a[contador] = numero;
+    };
+    contador += 1;
   }
-  println!("O total de ímpares é igual a: {}", total_impar);
-  println!("O percentual total é: {}", total_impar/100)
+
+  println!("O total de ímpares é igual a: {:?}", vetor_a);
+  // println!("O total de ímpares é igual a: {}", vetor_a.len());
+  // println!("O percentual total é: {}", vetor_a / 100);
+
+  fn converter_string_para_i32(uma_string: String) -> i32 {
+    uma_string
+      .trim()
+      .parse()
+      .expect("Falha ao converter o valor")
+  }
 }
+
+
+
